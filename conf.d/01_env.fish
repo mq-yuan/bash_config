9c668ad4e93a3704fa0703e426fc022b4f256817
mq-yuan <dearmengqiyuan@outlook.com>
2025-03-16 00:49:34 +0800

    add asdf and rustup; optimize env load;

--- conf.d/01_env.fish
+++ conf.d/01_env.fish
@@ -2,8 +2,9 @@
 # Set env by -U to keep same for all sessions
 set -U EDITOR "nvim"
 set -U fish_greeting "" # define your welcome message, you can set it as "" to ban it for better startup time.
+
 # load env config
-if test -f ~/.config/fish/.env
-    envsource ~/.config/fish/.env
+if test -f ~/.config/fish/.env.fish
+    source ~/.config/fish/.env.fish
 end
 
